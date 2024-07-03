9c27896b66982e1e61a36e5d309cb3b149621650
Jemorini <jemorini13@gmail.com>
2024-07-03 20:34:49 -0300

    feat: Adding dtos

--- src/controllers/email.controller.ts
+++ src/controllers/email.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { EmailService } from '../services/email.service';
+import { MessageDto } from 'src/dtos/message.dto';
 
 @Controller()
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post('send-email')
-  async sendEmail(): Promise<string> {
-    return await this.emailService.sendEmail();
+  async sendEmail(@Body() message: MessageDto): Promise<string> {
+    return await this.emailService.sendEmail(message);
   }
 }
